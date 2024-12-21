ef45fb09279a113a6a5b5c39b9af36ccd20acc76
noel2315 <noelraju2315.sse@saveetha.com>
2024-12-21 10:00:44 +0530

    Delete Add two numbers.py

--- Add two numbers.py
+++
@@ -1,9 +0,0 @@
-a = int(input())
-b = int(input())
-for x in range (a, b+1):
-    if x > 1:
-        for i in range (2, x):
-            if (x%i)== 0:
-                break
-        else:
-            print (x)
